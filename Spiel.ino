4cbdb8bb71775d93b23e89f21106a665955224ec
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2023-06-26 12:43:12 +0200

    Add files via upload

--- Spiel.ino
+++ Spiel.ino
@@ -1,10 +1,67 @@
 #include "FastLED.h"
+#include <LiquidCrystal.h>
 
 #define LEDPIN 8
 #define LEDCOUNT 256
 
+#define BUTTONPIN 7
+#define JOYSTICKX A2
+#define JOYSTICKY A3
+
+#define PLAYERS 1
+
 #define MAX_ITEMS 5
-#define MIN_ITEM_DELAY 5000
+#define MIN_ITEM_SPAWN_TICKS 10
+
+
+class Statistics {
+  public:
+    Statistics(LiquidCrystal* display);
+    void printPlayers();
+    void setPlayerScore(byte playerId, int score);
+    void playerDead(byte playerId);
+    void reset();
+  private:
+    LiquidCrystal* mDisplay;
+    int playerScores[PLAYERS];
+    bool playersDead[PLAYERS];
+
+};
+
+Statistics::Statistics(LiquidCrystal* display) {
+  mDisplay = display;
+  mDisplay->begin(16, 2);
+  mDisplay->clear();
+  reset();
+}
+
+void Statistics::printPlayers() {
+  mDisplay->clear();
+  for(int i = 0; i < PLAYERS; i++) {
+    mDisplay->write("Player ");
+    mDisplay->print(i + 1);
+    mDisplay->write(": ");
+    mDisplay->print(playerScores[i]);
+    if(playersDead[i]) {
+      mDisplay->write(" X");
+    }
+  }
+}
+
+void Statistics::setPlayerScore(byte playerId, int score) {
+  playerScores[playerId] = score;
+}
+
+void Statistics::playerDead(byte playerId) {
+  playersDead[playerId] = true;
+}
+
+void Statistics::reset() {
+  for(int i = 0; i < PLAYERS; i++) {
+    playerScores[i] = 0;
+    playersDead[i] = false;
+  }
+}
 
 class Matrix {
 
@@ -24,16 +81,29 @@ Matrix::Matrix() {
 }
 
 bool Matrix::setPixelColor(byte x, byte y, CRGB color, bool overwrite) {
-  if(x >= 16 || x < 0) return;
-  if(y >= 16 || y < 0) return;
+  if(x >= 16 || x < 0) return false;
+  if(y >= 16 || y < 0) return false;
+
+  int arrayPos = 16 * y + x;
 
   if(y % 2 == 0) {
-    leds[16 * y + (15 - x)] = color;
-
-  }else {
-    leds[16 * y + x] = color;
+    arrayPos = 16 * y + (15 - x);
   }
 
+  // if(y % 2 == 0) {
+  //   leds[16 * y + (15 - x)] = color;
+
+  // }else {
+  //   leds[16 * y + x] = color;
+  // }
+
+  CRGB off = 0x000000;
+
+  if(!overwrite && leds[arrayPos] != off) {
+    return false;
+  }
+
+  leds[arrayPos] = color;
   return true;
 }
 
@@ -52,7 +122,7 @@ class Item {
       mPosX = x;
       mPosY = y;
       mType = type;
-      return mDisplay->setPixelColor(x, y, 0xFF0000, false);
+      return mDisplay->setPixelColor(x, y, 0x0000FF, false);
     }
 
     bool exists() {
@@ -60,6 +130,11 @@ class Item {
     }
 
     bool checkCollission(byte x, byte y);
+
+    void remove() {
+      mPosX = 255;
+      mPosY = 255;
+    }
   private:
     Matrix* mDisplay;
     byte mPosX;
@@ -80,12 +155,25 @@ class Snake {
   public:
     Snake(Matrix* display,int initialLength, byte x, byte y, CRGB color);
     void reset();
+    void die() {
+      mDead = true;
+    }
+    bool dead() {
+      return mDead;
+    }
+
+    void blink();
+
     void initItems(Item* items) {
       gameItems = items;
     }
     bool moveForward();
     void turnRight();
     void turnLeft();
+
+    int getScore() {
+      return mLength - mStartLength;
+    }
   private:
     Matrix* mDisplay;
     CRGB mColor;
@@ -93,6 +181,9 @@ class Snake {
 
     byte mStartX;
     byte mStartY;
+
+    boolean mDead;
+    boolean mBlinkColor;
 
     int mStartLength;
     int mLength;
@@ -130,11 +221,24 @@ void Snake::reset() {
 
   mLength = mStartLength;
   mDirection = UP;
+  mDead = false;
+  mBlinkColor = false;
 
   for(int i = 0; i < mLength; i++) {
     mSnakePartsX[i] = mStartX;
     mSnakePartsY[i] = mStartY + i;
     mDisplay->setPixelColor(mSnakePartsX[i], mSnakePartsY[i], mColor, true);
+  }
+}
+
+void Snake::blink() {
+  mBlinkColor = !mBlinkColor;
+  for(int i = 0; i < mLength; i++) {
+    if(mBlinkColor) {
+      mDisplay->setPixelColor(mSnakePartsX[i], mSnakePartsY[i], 0xFF0000, true);
+    } else {
+      mDisplay->setPixelColor(mSnakePartsX[i], mSnakePartsY[i], mColor, true);
+    }
   }
 }
 
@@ -207,58 +311,110 @@ void Snake::turnLeft() {
 }
 
 Matrix m;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Snake snake(&m, 5, 4, 3, 0x00FF00);
+Statistics stats(&lcd);
 
-long lastGeneratedItem;
-long nextItemDelay;
+int ticksSinceLastItem;
+int nextItemTickCount;
 Item items[MAX_ITEMS](&m);
 
 bool gamePaused = true;
+long lastGameTick;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BUTTONPIN, INPUT);
   snake.initItems(items);
   m.show();
-  lastGeneratedItem = millis();
-  nextItemDelay = MIN_ITEM_DELAY;
+  ticksSinceLastItem = 0;
+  nextItemTickCount = MIN_ITEM_SPAWN_TICKS;
+  lastGameTick = millis();
 }
 
-void loop() {
+void gameTick() {
+  lastGameTick = millis();
 
-  //verhindert, dass bei pausiertem Spiel Items generiert werden
-  if(gamePaused) {
-    lastGeneratedItem = millis();
+
+  int readX = analogRead(JOYSTICKX);
+  int readY = analogRead(JOYSTICKY);
+
+  if(readX < 400) {
+    Serial.println("left");
+  }
+  if(readX > 600) {
+    Serial.println("right");
   }
 
-  if(millis() - lastGeneratedItem > nextItemDelay) {
+  if(readY < 400) {
+    Serial.println("up");
+  }
+
+  if(readY > 600) {
+    Serial.println("down");
+  }
+  
+
+  if(ticksSinceLastItem > nextItemTickCount) {
+    ticksSinceLastItem = 0;
+
     byte randX = random(0, 16);
     byte randY = random(0, 16);
     for(int i = 0; i < MAX_ITEMS; i++) {
       if(items[i].exists()) continue;
-      items[i].generateItem(randX, randY, 0);
+
+      boolean allowedGeneration = items[i].generateItem(randX, randY, 0);
+
+      if(!allowedGeneration) {
+        items[i].remove();
+      }
       break;
     }
+  }
+  ticksSinceLastItem++;
 
-    lastGeneratedItem = millis();
+
+  if(!gamePaused) {
+    bool survived = snake.moveForward();
+    stats.setPlayerScore(0, snake.getScore());
+    if(!survived) {
+      snake.die();
+      stats.playerDead(0);
+      gamePaused = true;
+    }
+    stats.printPlayers();
+  }
+
+  if(snake.dead()) {
+    snake.blink();
+  }
+  m.show();
+}
+
+void loop() {
+
+  if(gamePaused && digitalRead(BUTTONPIN)) {
+    Serial.println("start!");
+    snake.reset(); stats.reset(); stats.printPlayers();
+    gamePaused = false;
+    lastGameTick = millis();
+  }
+
+  //verhindert, dass bei pausiertem Spiel Items generiert werden
+  if(gamePaused) {
+    ticksSinceLastItem = 0;
   }
 
   if(Serial.available()) {
     char command = Serial.read();
     
     switch(command) {
-      case 's': gamePaused = false; break;
-      case 'g': snake.reset(); break;
       case 'r': snake.turnRight(); break;
       case 'l': snake.turnLeft(); break;
     }
   }
-  delay(500);
-  if(!gamePaused) {
-      bool survived = snake.moveForward();
-    if(!survived) {
-      Serial.println("failed");
-      gamePaused = true;
-    }
+
+  if(millis() - lastGameTick > 500) {
+    gameTick();
   }
-  m.show();
 }
